ba4c83d4dc74a5a548b00f80be9ccfca0c4abd02
eduardobagus <baguseduardo@gmail.com>
2025-02-24 12:11:41 +0700

    commit baru

---
+++ 02_Pengenalan_IDE_dan_Pemograman_NodeJS/variable-local-global.js
@@ -0,0 +1,14 @@
+let globalVariable = "Eduardo"
+
+function getMyIndenity() {
+    let localVariable = 2311104025
+
+    console.log(`Nama saya ${globalVariable} nim ${localVariable}`)
+}
+
+console.log(globalVariable)
+// console.log(localVariable)
+
+getMyIndenity()
+
+
---
+++ 02_Pengenalan_IDE_dan_Pemograman_NodeJS/variable.js
@@ -0,0 +1,14 @@
+var name = "Eduardo"
+const nim = 2311104025
+
+console.log(name)
+
+console.log("nama saya adalah " + name)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+var name = "Eduardo"
+
+let fullName = "Eduardo Bagus Prima Julian"
